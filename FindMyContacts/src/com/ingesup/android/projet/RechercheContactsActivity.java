dbddf2645b2662566d221050a2115fa92c1f4a28
frederic.daroussi <frederic.daroussi@gmail.com>
2012-11-09 16:13:53 +0000

    Ebauche des classes et fichiers xml (layout) pour les ecrans : 	* Login (page de test) 	* Terms & Conditions 	* Creation d'un nouveau contact 	* Profil du contact 	* Recherche d'un contact 	* Rapport

---
+++ FindMyContacts/src/com/ingesup/android/projet/RechercheContactsActivity.java
@@ -0,0 +1,14 @@
+package com.ingesup.android.projet;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class RechercheContactsActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recherche_contacts_layout);
+    }
+
+}
