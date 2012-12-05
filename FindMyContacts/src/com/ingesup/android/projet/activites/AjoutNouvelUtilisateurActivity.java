13e4e6a1dd73bbd1d5e1aa53a7966d55677d1adb
frederic.daroussi <frederic.daroussi@gmail.com>
2012-12-05 00:11:12 +0000

    Gestion activation/desactivation geolocalisation dans l'ecran "Ajout d'un nouvel utilisateur"

--- FindMyContacts/src/com/ingesup/android/projet/activites/AjoutNouvelUtilisateurActivity.java
+++ FindMyContacts/src/com/ingesup/android/projet/activites/AjoutNouvelUtilisateurActivity.java
@@ -6,12 +6,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class AjoutNouvelUtilisateurActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);		
 		setContentView(R.layout.nouvel_utilisateur_layout);
+		
+		final TextView vTfAdresse = (TextView) findViewById(R.id.tfAdresse);
+		final TextView vTfCodePostal = (TextView) findViewById(R.id.tfCodePostal);
+		final TextView vTfVille = (TextView) findViewById(R.id.tfVille);
+		final TextView vTfPays = (TextView) findViewById(R.id.tfPays);
+		ToggleButton vTogBtnGeoloc = (ToggleButton) findViewById(R.id.togBtnGeolocalisation);
+		
+		vTogBtnGeoloc.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if(isChecked) {
+					vTfAdresse.setVisibility(View.VISIBLE);
+					vTfCodePostal.setVisibility(View.VISIBLE);
+					vTfVille.setVisibility(View.VISIBLE);
+					vTfPays.setVisibility(View.VISIBLE);
+				}
+				else {
+					vTfAdresse.setVisibility(View.GONE);
+					vTfCodePostal.setVisibility(View.GONE);
+					vTfVille.setVisibility(View.GONE);
+					vTfPays.setVisibility(View.GONE);
+				}
+			}
+		});
 		
 		Button vBoutonCreerContact = (Button) findViewById(R.id.btnValiderContact);
 		vBoutonCreerContact.setOnClickListener(new OnClickListener() {
