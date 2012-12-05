52000ab935bb4c0acff195340e7ed5296babb8bd
frederic.daroussi@gmail.com <frederic.daroussi@gmail.com@e96669c4-4701-5258-8045-cbe0923e52e1>
2012-12-05 19:49:24 +0000

    Correction Issue #18 : Paramètres du serveur (IP_serveur:port) sauvegardés dans le téléphone

--- trunk/FindMyContacts/src/com/ingesup/android/projet/activites/FindMyContactsActivity.java
+++ trunk/FindMyContacts/src/com/ingesup/android/projet/activites/FindMyContactsActivity.java
@@ -14,7 +14,10 @@ import com.ingesup.android.projet.reponses.ReponseAuthentification;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -23,77 +26,91 @@ import android.widget.Toast;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
 public class FindMyContactsActivity extends Activity {
 
+	private SharedPreferences _preferences = null;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
         
+        // Issue #14 : récupération des préférences sauvegardées
+		_preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        
         Button vBoutonValider = (Button) findViewById(R.id.btnValiderLogin);
         vBoutonValider.setOnClickListener(new OnClickListener() {
 			
 			public void onClick(View v) {
+				String vIpServeur = _preferences.getString("SERVEUR", "");
+				Log.d(FindMyContactsActivity.class.toString(), "IP Serveur : " + vIpServeur);
 				
-				// recuperation des valeurs des champs texte
-				TextView vChampLogin =  (TextView) findViewById(R.id.champLogin);
-				TextView vChampMotDePasse = (TextView) findViewById(R.id.champMotDePasse);
 				TextView vTexteMessageErreur = (TextView) findViewById(R.id.texteErreurLogin);
-				vTexteMessageErreur.setText("");
-				
-				if(isNetworkAvailable()) {
-					// Creation du message JSON pour l'authentification
-					String vLogin = vChampLogin.getText().toString();
-					
-					JSONObject vMessageJSON = FormatMessageEnvoi
-							.formatterMessageLogin(
-									vLogin,
-									vChampMotDePasse.getText().toString());
-					
-					// Envoi du message en tache de fond
-					GestionMessage vGestionnaireMessage = new GestionMessage();
-					vGestionnaireMessage.execute(
-							"http://192.168.0.71:8080/ab_service_mgr/api/mobile/login",	// home
-//							"http://10.10.160.230:8080/ab_service_mgr/api/mobile/login",	// school
-//							"http://10.68.218.19:8080/ab_service_mgr/api/mobile/login",	// work
-							vMessageJSON.toString());
-					
-					// Lecture du message reponse
-					try {						
-						JSONObject vMessageReponse = vGestionnaireMessage.get(10, TimeUnit.SECONDS);	// attente du message reponse
-						
-						if(vMessageReponse != null) {
-							ReponseAuthentification vMessageReponseAuthentification = 
-									AnalyseurMessageReception.analyserReponseAuthentification(vMessageReponse);
-							
-							if((vMessageReponseAuthentification != null) && (vMessageReponseAuthentification.estAuthentifie())) {
-								Toast.makeText(FindMyContactsActivity.this, vLogin + " connecté !", Toast.LENGTH_SHORT).show();
-								Intent vIntent = new Intent(FindMyContactsActivity.this, ProfilUtilisateurActivity.class);
-								vIntent.putExtra("login", vLogin);
-								vIntent.putExtra("jeton", vMessageReponseAuthentification.getJetonSession());
-								startActivity(vIntent);
-							}
-							else {
-								vTexteMessageErreur.setText("Login/Mot de passe incorrect");
-							}
-						}
-						else {
-							Log.e(FindMyContactsActivity.class.toString(), "Pas de message reponse retourné.");
-						}
-						
-					} catch (InterruptedException e) {
-						Log.e(FindMyContactsActivity.class.toString(), "Tache d'envoi de message de connexion interrompue : " + e.getMessage());
-					} catch (ExecutionException e) {
-						Log.e(FindMyContactsActivity.class.toString(), "Impossible de recuperer le message reponse JSON : " + e.getMessage());
-					} catch (TimeoutException e) {
-						Log.e(FindMyContactsActivity.class.toString(), "Timeout atteint...");
-						vTexteMessageErreur.setText("Serveur indisponible");						
-					}
+
+				if(vIpServeur.equals("")) {
+					Log.e(FindMyContactsActivity.class.toString(), "Timeout atteint...");
+					vTexteMessageErreur.setText("Serveur non défini.\nAppuyer sur \'MENU\' puis \"Préférences\" pour changer l'adresse IP du serveur");	
 				}
 				else {
-					Toast.makeText(FindMyContactsActivity.this, "Vérifier votre connexion reseau", Toast.LENGTH_LONG).show();
-					Log.d(FindMyContactsActivity.class.toString(), "Vérifier votre connexion reseau");
+					// recuperation des valeurs des champs texte
+					TextView vChampLogin =  (TextView) findViewById(R.id.champLogin);
+					TextView vChampMotDePasse = (TextView) findViewById(R.id.champMotDePasse);
+					vTexteMessageErreur.setText("");
+					
+					if(isNetworkAvailable()) {
+						// Creation du message JSON pour l'authentification
+						String vLogin = vChampLogin.getText().toString();
+						
+						JSONObject vMessageJSON = FormatMessageEnvoi
+								.formatterMessageLogin(
+										vLogin,
+										vChampMotDePasse.getText().toString());
+						
+						// Envoi du message en tache de fond
+						GestionMessage vGestionnaireMessage = new GestionMessage();
+						vGestionnaireMessage.execute(
+								"http://" + vIpServeur + "/ab_service_mgr/api/mobile/login",
+								vMessageJSON.toString());
+						
+						// Lecture du message reponse
+						try {						
+							JSONObject vMessageReponse = vGestionnaireMessage.get(10, TimeUnit.SECONDS);	// attente du message reponse
+							
+							if(vMessageReponse != null) {
+								ReponseAuthentification vMessageReponseAuthentification = 
+										AnalyseurMessageReception.analyserReponseAuthentification(vMessageReponse);
+								
+								if((vMessageReponseAuthentification != null) && (vMessageReponseAuthentification.estAuthentifie())) {
+									Toast.makeText(FindMyContactsActivity.this, vLogin + " connecté !", Toast.LENGTH_SHORT).show();
+									Intent vIntent = new Intent(FindMyContactsActivity.this, ProfilUtilisateurActivity.class);
+									vIntent.putExtra("login", vLogin);
+									vIntent.putExtra("jeton", vMessageReponseAuthentification.getJetonSession());
+									vIntent.putExtra("serveur", vIpServeur);
+									startActivity(vIntent);
+								}
+								else {
+									vTexteMessageErreur.setText("Login/Mot de passe incorrect");
+								}
+							}
+							else {
+								Log.e(FindMyContactsActivity.class.toString(), "Pas de message reponse retourné.");
+							}
+							
+						} catch (InterruptedException e) {
+							Log.e(FindMyContactsActivity.class.toString(), "Tache d'envoi de message de connexion interrompue : " + e.getMessage());
+						} catch (ExecutionException e) {
+							Log.e(FindMyContactsActivity.class.toString(), "Impossible de recuperer le message reponse JSON : " + e.getMessage());
+						} catch (TimeoutException e) {
+							Log.e(FindMyContactsActivity.class.toString(), "Timeout atteint...");
+							vTexteMessageErreur.setText("Serveur indisponible");						
+						}
+					}
+					else {
+						Toast.makeText(FindMyContactsActivity.this, "Vérifier votre connexion reseau", Toast.LENGTH_LONG).show();
+						Log.d(FindMyContactsActivity.class.toString(), "Vérifier votre connexion reseau");
+					}
 				}
 			}
 		});
@@ -111,4 +128,24 @@ public class FindMyContactsActivity extends Activity {
         return false;
     } 
     
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_preferences, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+			case R.id.preferences: {// Issue #18 (FDA) : Gestion des préférences de l'application
+				Intent vIntent = new Intent(FindMyContactsActivity.this, PreferencesActivity.class);
+				startActivity(vIntent);
+				break;	
+			}	
+
+    		default : Log.e(ProfilUtilisateurActivity.class.toString(), "Menu inconnu : " + item.getItemId());
+		}
+    	
+    	return super.onOptionsItemSelected(item);
+    }
 }
